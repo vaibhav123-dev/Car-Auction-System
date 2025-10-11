0921a3d16f8ea43194f77f2756d91845a1390adc
Vaibhav Darvekar <93373467+vaibhav123-dev@users.noreply.github.com>
2025-10-11 19:05:11 +0530

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import userRoutes from './user.routes.js';
 import auctionRoutes from './auction.routes.js';
 import carRoutes from './car.routes.js';
+import bidRoutes from './bid.routes.js';
 
 const router = express.Router();
 // import carRoutes from './car.routes.js';
@@ -9,5 +10,6 @@ const router = express.Router();
 router.use('/user', userRoutes);
 router.use('/auction', auctionRoutes);
 router.use('/car', carRoutes);
+router.use('/bid' bidRoutes)
 
 export default router;
